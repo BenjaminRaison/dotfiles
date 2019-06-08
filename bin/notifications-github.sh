f24d0b89f86e39454e6b9df475f5e4e9ab2a1a42
Benjamin Raison <benjamin.raison@benjaminraison.eu>
2019-06-08 18:57:02 +0200

    Fix shellcheck warning

--- bin/notifications-github.sh
+++ bin/notifications-github.sh
@@ -3,7 +3,7 @@
 
 TOKEN="$(cat ~/.secrets/waybar-github-token)"
 
-notifications=$(curl -fs https://api.github.com/notifications?access_token=$TOKEN | jq ".[].unread" | grep -c true)
+notifications=$(curl -fs https://api.github.com/notifications?access_token="$TOKEN" | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
     echo "$notifications"
