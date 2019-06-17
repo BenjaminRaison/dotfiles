cf82f76af88d0124a48a8f4cca7b6683425c50c8
Benjamin Raison <benjamin.raison@benjaminraison.eu>
2019-06-17 08:36:07 +0200

    Make notification icons blend in better

--- bin/notifications-github.sh
+++ bin/notifications-github.sh
@@ -6,7 +6,7 @@ TOKEN="$(cat ~/.secrets/waybar-github-token)"
 notifications=$(curl -fs https://api.github.com/notifications?access_token="$TOKEN" | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
-    echo "$notifications"
+    echo " $notifications"
 else
     echo ""
 fi
